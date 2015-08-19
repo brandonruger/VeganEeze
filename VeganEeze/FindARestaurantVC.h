21f4ab3654f876fb16e75f3cc363b6493eac008a
r02548 <bruger@fullsail.edu>
2015-08-18 22:50:46 -0400

    Setup find a restaurant/search events pages

---
+++ VeganEeze/FindARestaurantVC.h
@@ -0,0 +1,13 @@
+//
+//  FindARestaurantVC.h
+//  VeganEeze
+//
+//  Created by Brandon Ruger on 8/18/15.
+//  Copyright (c) 2015 Brandon Ruger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindARestaurantVC : UIViewController
+
+@end
---
+++ VeganEeze/SearchEventsVC.h
@@ -0,0 +1,13 @@
+//
+//  SearchEventsVC.h
+//  VeganEeze
+//
+//  Created by Brandon Ruger on 8/18/15.
+//  Copyright (c) 2015 Brandon Ruger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchEventsVC : UIViewController
+
+@end
