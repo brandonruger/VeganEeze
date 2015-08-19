68d039e8f22262b4c5b67914bf155596ddda5e6b
r02548 <bruger@fullsail.edu>
2015-08-18 21:05:18 -0400

    Setup project on Xcode

---
+++ VeganEeze/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VeganEeze
+//
+//  Created by Brandon Ruger on 8/18/15.
+//  Copyright (c) 2015 Brandon Ruger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VeganEeze/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VeganEeze
+//
+//  Created by Brandon Ruger on 8/18/15.
+//  Copyright (c) 2015 Brandon Ruger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
