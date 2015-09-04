cbccb1aad5550b1216ef51b1f333a228b4e0638e
r02548 <bruger@fullsail.edu>
2015-09-03 20:48:40 -0400

    Fixed issue with app crashing when trying to load

--- VeganEeze/AppDelegate.m
+++ VeganEeze/AppDelegate.m
@@ -28,23 +28,23 @@
     // [Optional] Track statistics around application opens.
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     
-    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
-    UIStoryboard *veganEezeSB = [UIStoryboard storyboardWithName:@"VeganEezeStoryboard" bundle:nil];
-    
-    //Check if user is currently logged in
-    PFUser *loggedInUser = [PFUser currentUser];
-    if (loggedInUser) {
-        //User is logged in, make Main Menu the root view controller
-        UIViewController *mainMenuVC = [veganEezeSB instantiateViewControllerWithIdentifier:@"MainMenuViewController"];
-        self.window.rootViewController = mainMenuVC;
-        [self.window makeKeyAndVisible];
-        
-    } else {
-        //User is not logged in, make login screen the root view controller
-        UIViewController *loginVC = [veganEezeSB instantiateViewControllerWithIdentifier:@"LoginViewController"];
-        self.window.rootViewController = loginVC;
-        [self.window makeKeyAndVisible];
-    }
+//    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+//    UIStoryboard *veganEezeSB = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
+//    
+//    //Check if user is currently logged in
+//    PFUser *loggedInUser = [PFUser currentUser];
+//    if (loggedInUser) {
+//        //User is logged in, make Main Menu the root view controller
+//        UIViewController *mainMenuVC = [veganEezeSB instantiateViewControllerWithIdentifier:@"MainMenuViewController"];
+//        self.window.rootViewController = mainMenuVC;
+//        [self.window makeKeyAndVisible];
+//        
+//    } else {
+//        //User is not logged in, make login screen the root view controller
+//        UIViewController *loginVC = [veganEezeSB instantiateViewControllerWithIdentifier:@"LoginViewController"];
+//        self.window.rootViewController = loginVC;
+//        [self.window makeKeyAndVisible];
+//    }
     
     
     return YES;
