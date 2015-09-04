0fe9c7d9c8879cd7f202e3f9a4baa46ad0df0004
r02548 <bruger@fullsail.edu>
2015-09-03 21:04:08 -0400

    Revert commit

--- VeganEeze/BeverageSearchVC.m
+++ VeganEeze/BeverageSearchVC.m
@@ -130,32 +130,17 @@
     //NSDictionary *firstItemRetrieved = [arrayOfJSONData objectAtIndex:0];
     //NSLog(@"firstItem = %@", [firstItemRetrieved description]);
     
-    //Check to make sure results were found
-    if (arrayOfJSONData.count == 0) {
-        //No results found, alert user
-        //Alert user no results were found
-        UIAlertView *noResults = [[UIAlertView alloc]initWithTitle:@"Error" message:@"No results found. Please revise your search and try again." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-        [noResults show];
-    } else {
-        
-        //Loop through all results retrieved from API call
-        for (int i=0; i<[arrayOfJSONData count]; i++) {
-            //Use custom method to grab each object from dictionary and add each object to the NSMutableArray
-            AlcoholBeverage *alcoholBevDetails = [self createAlcoholBeverageObjects:[arrayOfJSONData objectAtIndex:i]];
-            if (alcoholBevDetails != nil) {
-                //Add object to array
-                [alcoholBeverageObjects addObject:alcoholBevDetails];
-            }
+    //Loop through all results retrieved from API call
+    for (int i=0; i<[arrayOfJSONData count]; i++) {
+        //Use custom method to grab each object from dictionary and add each object to the NSMutableArray
+        AlcoholBeverage *alcoholBevDetails = [self createAlcoholBeverageObjects:[arrayOfJSONData objectAtIndex:i]];
+        if (alcoholBevDetails != nil) {
+            //Add object to array
+            [alcoholBeverageObjects addObject:alcoholBevDetails];
         }
-        
-        BeverageResultsTVC *bevResultsTVC = [self.storyboard instantiateViewControllerWithIdentifier:@"BeverageResultsViewController"];
-        //Pass the array of AlcoholBeverage objects to the Beverage Results vc
-        bevResultsTVC.arrayOfAlcoholBeverages = alcoholBeverageObjects;
-        //Instantiate new view controller
-        [self.navigationController pushViewController:bevResultsTVC animated:YES];
-        
     }
     
+<<<<<<< HEAD
 //    //Loop through all results retrieved from API call
 //    for (int i=0; i<[arrayOfJSONData count]; i++) {
 //        //Use custom method to grab each object from dictionary and add each object to the NSMutableArray
@@ -171,6 +156,13 @@
 //    bevResultsTVC.arrayOfAlcoholBeverages = alcoholBeverageObjects;
 //    //Instantiate new view controller
 //    [self.navigationController pushViewController:bevResultsTVC animated:YES];
+=======
+    BeverageResultsTVC *bevResultsTVC = [self.storyboard instantiateViewControllerWithIdentifier:@"BeverageResultsViewController"];
+    //Pass the array of AlcoholBeverage objects to the Beverage Results vc
+    bevResultsTVC.arrayOfAlcoholBeverages = alcoholBeverageObjects;
+    //Instantiate new view controller
+    [self.navigationController pushViewController:bevResultsTVC animated:YES];
+>>>>>>> parent of cc494da... Fixed beverage search so it alerts user when no results found
 
     
     //Set bool to true since data retrieval is complete
