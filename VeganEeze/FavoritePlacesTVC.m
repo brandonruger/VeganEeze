dc0e9abae619c7a7d63c286d76b4586e755fd5f0
r02548 <bruger@fullsail.edu>
2015-09-12 12:34:58 -0400

    Added alternating row colors to favorite places/places to visit

--- VeganEeze/FavoritePlacesTVC.m
+++ VeganEeze/FavoritePlacesTVC.m
@@ -78,6 +78,19 @@
         resultsCell.detailTextLabel.text = [placeCityState objectAtIndex:indexPath.row];
     }
     
+    //Alternate color for every other row
+    if (indexPath.row %2 == 0) {
+        
+        UIColor *alternateColor=[[UIColor alloc]initWithRed:239.0/255.0 green:252.0/255.0 blue:214.0/255.0 alpha:1];
+        //resultsCell.backgroundColor = [UIColor clearColor];
+        resultsCell.backgroundColor = alternateColor;
+    } else {
+        UIColor *otherColor=[[UIColor alloc]initWithRed:162.0/255.0 green:201.0/255.0 blue:142.0/255.0 alpha:1];
+        //resultsCell.backgroundColor = [UIColor clearColor];
+        resultsCell.backgroundColor = otherColor;
+    }
+
+    
     return resultsCell;
 }
 
